08093a4233047a2f3e1674a7972a60c37e146430
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 04:01:44 -0700

    new version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var transformer = require('dat-transformer');
-var tAscii = transformer('ascii');
-var tBuffer = transformer('buffer');
+var Conversion = require('transformer-conversion');
+var tAscii = require('transformer.ascii');
+var tBuffer = require('transformer.buffer');
 // require any other modules you may need here.
 
-module.exports = transformer.Conversion(tAscii, tBuffer, convert);
+module.exports = Conversion(tAscii, tBuffer, convert);
 
 function convert(ascii) {
   return new Buffer(ascii, 'ascii');
